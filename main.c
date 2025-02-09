9134fcf73748e063fe7c50723f557223f6eb2861
Stefan Berecz <108223133+StefanBerecz@users.noreply.github.com>
2025-02-09 22:10:55 +0100

    Add automatic class, net- & broadcast-address and usable range
    closes #1; Added automatic class calculation, netaddress, broadcastaddress and usable host range

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include <ctype.h>
 
+void calcClass(char *ipAddr, char *class)
+{
+    //Calculate class from IP Address
+    int block1;
+    sscanf(ipAddr, "%d", &block1);
+    if(block1 >= 1 && block1 <= 127)
+        *class = 'A';
+    else if(block1 >= 128 && block1 <= 191)
+        *class = 'B';
+    else if(block1 >= 192 && block1 <= 223)
+        *class = 'C';
+    else
+        printf("Invalid IP Address\n");
+}
+
 int checkCidrClass(char class, int cidr)
 {
     //Check if CIDR is valid for the class
@@ -31,9 +46,9 @@ char* calcnetmask(int cidr)
     static char netmask[16];
     int block1, block2, block3, block4;
     //Convert CIDR to netmask
-    if(cidr <= 8)
+    if(cidr == 8)
     {
-        block1 = 256 - (1 << (8 - cidr));
+        block1 = 255;  //256 - (1 << (8 - cidr))
         block2 = 0;
         block3 = 0;
         block4 = 0;
@@ -63,6 +78,106 @@ char* calcnetmask(int cidr)
     return netmask;
 }
 
+void calcNetBroadRange(char *ipAddr, int cidr, char *netAddr, char *broadCAddr, char *usableRange)
+{
+    int ipBlock1, ipBlock2, ipBlock3, ipBlock4;
+    sscanf(ipAddr, "%d.%d.%d.%d", &ipBlock1, &ipBlock2, &ipBlock3, &ipBlock4);
+
+    int netmask1, netmask2, netmask3, netmask4;
+    sscanf(calcnetmask(cidr), "%d.%d.%d.%d", &netmask1, &netmask2, &netmask3, &netmask4);
+
+    //Calculate network address
+    int netBlock1, netBlock2, netBlock3, netBlock4;
+    netBlock1 = ipBlock1 & netmask1; //Bitwise AND
+    netBlock2 = ipBlock2 & netmask2;
+    netBlock3 = ipBlock3 & netmask3;
+    netBlock4 = ipBlock4 & netmask4;
+    sprintf(netAddr, "%d.%d.%d.%d", netBlock1, netBlock2, netBlock3, netBlock4);
+
+    //Calculate broadcast address
+    int broadBlock1, broadBlock2, broadBlock3, broadBlock4;
+    broadBlock1 = netBlock1 | ~netmask1 & 0xFF; //Bitwise OR with inverted netmask and mask with 0xFF to get last octet
+    broadBlock2 = netBlock2 | ~netmask2 & 0xFF;
+    broadBlock3 = netBlock3 | ~netmask3 & 0xFF;
+    broadBlock4 = netBlock4 | ~netmask4 & 0xFF;
+    sprintf(broadCAddr, "%d.%d.%d.%d", broadBlock1, broadBlock2, broadBlock3, broadBlock4);
+
+    //Calculate usable range 
+    if(netBlock4 == 255)
+    {
+        if(netBlock3 == 255)
+        {
+            if(netBlock2 == 255)
+            {
+                if(netBlock1 == 255)
+                {
+                    printf("No usable range\n");
+                }
+                else
+                {
+                    netBlock1++;
+                    netBlock2 = 0;
+                    netBlock3 = 0;
+                    netBlock4 = 0;
+                }
+            }
+            else
+            {
+                netBlock2++;
+                netBlock3 = 0;
+                netBlock4 = 0;
+            }
+        }
+        else
+        {
+            netBlock3++;
+            netBlock4 = 0;
+        }
+    }
+    else
+    {
+        netBlock4++;
+    }
+
+    if(broadBlock4 == 0)
+    {
+        if(broadBlock3 == 0)
+        {
+            if(broadBlock2 == 0)
+            {
+                if(broadBlock1 == 0)
+                {
+                    printf("No usable range\n");
+                }
+                else
+                {
+                    broadBlock1--;
+                    broadBlock2 = 255;
+                    broadBlock3 = 255;
+                    broadBlock4 = 255;
+                }
+            }
+            else
+            {
+                broadBlock2--;
+                broadBlock3 = 255;
+                broadBlock4 = 255;
+            }
+        }
+        else
+        {
+            broadBlock3--;
+            broadBlock4 = 255;
+        }
+    }
+    else
+    {
+        broadBlock4--;
+    }
+
+    sprintf(usableRange, "%d.%d.%d.%d - %d.%d.%d.%d", netBlock1, netBlock2, netBlock3, netBlock4, broadBlock1, broadBlock2, broadBlock3, broadBlock4);
+}
+
 void calcSubs(char class, int cidr, int *subnetptr)
 {
     //Calculate number of subnets
@@ -89,10 +204,11 @@ int main()
     char netAddr[16];
     char broadCAddr[16];
     char netmask[16];
+    char usableRange[34];
     char class;
     while(1)
     {
-        printf("Enter Class (A, B, C) (press q to quit): \n");
+        printf("Enter Class (A, B, C) (press q to quit, x for automatic mode): \n");
         scanf(" %c", &class);
         class = toupper(class);
         switch(class)
@@ -100,6 +216,7 @@ int main()
             case 'A':
             case 'B':
             case 'C':
+            case 'X':
                 break;
             case 'Q':
                 return 0;
@@ -116,6 +233,9 @@ int main()
             continue;
         }
 
+        if(class == 'X')
+            calcClass(ipAddr, &class);
+
         printf("Enter CIDR: \n");
         scanf("%2d", &cidr);
         if(checkCidrClass(class, cidr) == 1)
@@ -129,9 +249,14 @@ int main()
         strcpy(netmask,calcnetmask(cidr));
         printf("Netmask: %s\n", netmask);
         
-        calcSubs(class, cidr, subnetptr);
         printf("Number of Hosts: %d usable: %d\n", 1 << (32 - cidr), (1 << (32 - cidr)) - 2); // 2 subtracted for network and broadcast address
 
+        calcNetBroadRange(ipAddr, cidr, netAddr, broadCAddr, usableRange);
+        printf("Network Address: %s\n", netAddr);
+        printf("Broadcast Address: %s\n", broadCAddr);
+        printf("Usable Range: %s\n", usableRange);
+
+        calcSubs(class, cidr, subnetptr);
         if(subnets < 2)
             printf("No Subnets, default network only\n");
         else
